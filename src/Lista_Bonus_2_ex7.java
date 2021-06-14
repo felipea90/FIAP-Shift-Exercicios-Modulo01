b26855c8d18c9bb9fc557f17d01a2af591929cd2
Felipe Amaral <felipe.abcosta90@gmail.com>
2021-06-14 18:19:07 -0300

    Calcular notas acima e abaixo da media

--- src/Lista_Bonus_2_ex7.java
+++ src/Lista_Bonus_2_ex7.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 
-public class Lista_Bonus_2_ex7 {
+public class Lista_Bonus_2_ex7 
+{
 	
 	//7 – No parque Valter Dino World, os clientes podem ter a chance de passar uma noite no castelo da princesa Javarella.
 	
 	// Para isso, devem acertar quantos feijões estão em um jarro (1238). Faça um programa que solicite a digitação da quantidade de feijões até que um cliente acerte.
 
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
 		Scanner leitor = new Scanner(System.in);
 		
@@ -20,13 +22,14 @@ public class Lista_Bonus_2_ex7 {
 		System.out.println("Digite o número de feijões:");
 		feijoes = leitor.nextInt();
 		
-		while (feijoes != 1238)
-		{
-			System.out.println("Você errou!\nDigite o número de feijões:");
-			feijoes = leitor.nextInt();
-		}
-		
-		System.out.println("Você acertou!\nHaviam 1238 feijões.\nBusque seu cupom na cabana do Dino!");
+			while (feijoes != 1238)
+			{
+				System.out.println("Você errou!\nDigite o número de feijões:");
+				feijoes = leitor.nextInt();
+			}
+			
+			System.out.println("Você acertou!\nHaviam 1238 feijões.\nBusque seu cupom na cabana do Dino!");
+				
 		
 		leitor.close();
 
---
+++ src/Lista_Bonus_2_ex8.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Lista_Bonus_2_ex8 {
+
+	// 8 – Um professor quer saber quantos alunos de uma sala de 5 tiveram nota maior do que a média. Faça um programa onde o professor informe a média e as notas de cada um dos 5 alunos e, ao final, seja exibido quantos alunos tiveram nota superior à média e quantos tiveram nota inferior à média.
+	
+	public static void main(String[] args) {
+
+		Scanner leitor = new Scanner(System.in);
+		
+		
+		double media, nota;
+		int contador=0, abaixo=0, acima=0;
+		
+		System.out.println("Programa que calcula a quantidade de alunos acima e abaixo da média");
+		
+		System.out.println("Professor, entre com a média de corte:");
+		media = leitor.nextDouble();
+		
+		while (contador < 5)
+		{
+			System.out.println("Digite a nota do aluno");
+			nota = leitor.nextDouble();
+			
+			if (nota < media) {
+				abaixo = abaixo + 1;
+			}
+			else {
+				acima = acima + 1;
+			}
+			
+			contador = contador + 1;
+		}
+		
+		System.out.println("A quantidade de alunos acima da média " + media + " é de: " + acima);
+		
+		System.out.println("A quantidade de alunos abaixo da média " + media + " é de: " + abaixo);
+		
+		leitor.close();
+
+	}
+
+}
