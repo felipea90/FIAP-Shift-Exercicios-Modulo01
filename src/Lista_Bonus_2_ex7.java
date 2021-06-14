98760e8686aa15684bcc8286ef4cef44caace88f
Felipe Amaral <felipe.abcosta90@gmail.com>
2021-06-14 17:20:47 -0300

    Sorteio do Castelo da Princesa

--- src/Lista_Bonus_2_ex7.java
+++ src/Lista_Bonus_2_ex7.java
@@ -11,7 +11,22 @@ public class Lista_Bonus_2_ex7 {
 		
 		Scanner leitor = new Scanner(System.in);
 		
+		int feijoes;
 		
+		System.out.println("Programa de sorteio do castelo da princesa\n");
+		
+		System.out.println("Acerte a quantidade de feijões no Jarro\n");
+		
+		System.out.println("Digite o número de feijões:");
+		feijoes = leitor.nextInt();
+		
+		while (feijoes != 1238)
+		{
+			System.out.println("Você errou!\nDigite o número de feijões:");
+			feijoes = leitor.nextInt();
+		}
+		
+		System.out.println("Você acertou!\nHaviam 1238 feijões.\nBusque seu cupom na cabana do Dino!");
 		
 		leitor.close();
 
